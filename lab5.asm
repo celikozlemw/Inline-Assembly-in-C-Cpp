1a188d2c8827941a3c9a3683ae0307c9e6cab7cc
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-12-27 14:54:56 +0300

    Update and rename a.asm to lab5.asm

--- lab5.asm
+++ lab5.asm
@@ -75,3 +75,23 @@ Program, ADC10_ISR etiketi ile devam eder. Bu bölümde, ADC10 kesmesinin gerçe
 Program, Mainloop bölümüne geri döner. Bu bölümde, ADC10MEM kaydındaki değer 007Fh ile karşılaştırılır. ADC10MEM, ADC10 tarafından dönüştürülen dijital değeri tutan bir kayıttır4. Eğer ADC10MEM değeri 007Fh’den büyük veya eşit ise, program aa etiketine atlar. Aksi halde, program Mainloop bölümünün başına döner.
 Program, aa etiketi ile devam eder. Bu bölümde, ADC10MEM kaydındaki değer 01FFh ile karşılaştırılır. Eğer ADC10MEM değeri 01FFh’den küçük ise, program Mainloop bölümünün başına döner. Aksi halde, P1.0 pininin değeri 1 olarak ayarlanır, yani LED yanar. Sonra, program Mainloop bölümünün başına döner.
 Program, son bölümde kesme vektörlerini tanımlar. Kesme vektörleri, kesmelerin hangi adreslerdeki kodları çalıştıracağını belirleyen kayıtlardır5. Bu programda, reset kesmesi RESET etiketindeki kodu, ADC10 kesmesi ise ADC10_ISR etiketindeki kodu çalıştırır.
+
+Programın başında, P1.0 ve P1.6 pinleri çıkış olarak ayarlanır. Bu pinler, LED’leri yakmak veya söndürmek için kullanılır. P1.1 pin ise giriş olarak ayarlanır. Bu pin, potansiyometrenin orta ucu ile bağlanır. Potansiyometre, ADC dönüşümü için analog sinyal sağlar. 
+
+Programın ana döngüsünde, ADC10CTL0 ve ADC10CTL1 kayıtları, ADC modülünü ayarlamak için kullanılır. ADC10CTL0 kaydı, ADC’yi etkinleştirir, kesmeleri izin verir, örnekleme süresini belirler ve dönüşümü başlatır. ADC10CTL1 kaydı, ADC giriş kanalını seçer, saat kaynağını belirler ve dönüşüm modunu ayarlar. 
+
+ADC dönüşümü tamamlandığında, ADC10MEM kaydındaki sonuç, 0x220 adresinden başlayarak bellekteki bir diziye kaydedilir. Bu işlem, ADC10_ISR kesme alt proğramında gerçekleştirilir. Kesme alt proğramında, R15 kaydı, dizi indeksi olarak kullanılır. R15 kaydı, her dönüşüm sonrası 2 arttırılır ve 0x1F ile maskelenir. Bu şekilde, dizi boyutu 16 olarak sınırlanır. Kesme alt proğramından çıkarken, CPUOFF biti sıfırlanır ve ana döngüye dönülür. 
+
+Ana döngüde, ADC10MEM kaydındaki sonuç, 0x1FF ile karşılaştırılır. Eğer sonuç 0x1FF’den büyükse, P1.0 pinindeki LED yakılır. Eğer sonuç 0x7F’den küçükse, P1.6 pinindeki LED yakılır. Eğer sonuç bu aralıkta ise, her iki LED de söndürülür. Bu işlem, ADC dönüşümü sürekli olarak tekrarlanır. 
+
+ 
+
+ 
+
+ 
+
+Potansiyometreyi çevirdiğinizde, ADC10MEM kaydındaki değerin değiştiğini gözlemleyin. Potansiyometreyi maksimum veya minimum değere getirdiğinizde, ADC10MEM kaydındaki değerin 0x3FF veya 0x000 olduğunu gözlenir. 
+
+ADC10MEM kaydındaki değerin, bellekteki diziye doğru şekilde kaydedildiğini gözlemleyin. Bellek penceresinden 0x220 adresinden itibaren olan değerleri inceleyin. Her dönüşüm sonrası, bir sonraki adrese yeni bir değer yazıldığını gözlemleyin. Dizi boyutunun 16 olduğunu ve döngüsel olarak yazıldığını gözlenir. 
+
+ADC10MEM kaydındaki değere göre, LED’lerin doğru şekilde yakıldığını veya söndürüldüğünü gözlemleyin. Potansiyometreyi çevirdiğinizde, LED’lerin durumunun değiştiğini gözlemleyin. Potansiyometreyi orta değere getirdiğinizde, her iki LED’in de söndüğünü gözlenir
